b58aca4ae85067f30155178748caf032395978ee
bkumar2018 <birender.cdac@gmail.com>
2024-04-15 18:31:04 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const studentRoutes = require('./src/student/routes');
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.send("Hello, welcome to student registration portal! For Registration use URI - /api/v1/students");
+});
+
+app.use("/api/v1/students", studentRoutes);
+
+app.listen(port, () => console.log('app listening on port ${port}'));
